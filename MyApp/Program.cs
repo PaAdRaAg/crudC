97cd0bf34514ab6737682edd89239af1d26ae022
PaAdRaAg <54459507+PaAdRaAg@users.noreply.github.com>
2023-04-26 00:20:21 -0600

    Delete MyApp directory

--- MyApp/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-// Console.WriteLine("Hello, World!");
-
-Console.WriteLine("The current time is " + DateTime.Now);
-
-Console.WriteLine("Congratulations!");
-Console.WriteLine("You wrote your first lines of code!");
